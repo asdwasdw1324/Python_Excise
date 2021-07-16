4e24cb63861eb9769e5143295f1450cbd23eb195
ATmingfan <80809662+ATmingfan@users.noreply.github.com>
2021-07-16 10:30:29 +0800

    Add files via upload

---
+++ test.py.py
@@ -0,0 +1,30 @@
+#Case1_collatz
+def collatz(number):
+    if number%2==0:
+        print(number//2)
+        n=number//2
+    else:
+        print(3*number+1)
+        n=3*number+1
+    return n
+#定义collatz函数
+print("Please enter a number")
+EnteredNumber=int(input())
+if EnteredNumber==1:
+    print("The number you entered first can not 1")
+    print("Please entered the number again")
+    EnteredNumber=int(input())
+    while EnteredNumber==1:
+        print("The number you entered first can not 1")
+        print("Please entered the number again")
+        EnteredNumber=int(input())
+        continue
+    while EnteredNumber!=1:
+        EnteredNumber=collatz(EnteredNumber)
+        continue
+    print("Well,the number you entered has been calculated to 1 finally")
+else:
+    while EnteredNumber!=1:
+        EnteredNumber=collatz(EnteredNumber)
+        continue
+    print("Well,the number you entered has been calculated to 1 finally")
