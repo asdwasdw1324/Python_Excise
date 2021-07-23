35c3f03ae95358d135f7b5cabc4bf754fc8eb956
ATmingfan <80809662+ATmingfan@users.noreply.github.com>
2021-07-23 22:45:41 +0800

    Update test.py.py

--- test.py.py
+++ test.py.py
@@ -19,3 +19,17 @@ while EnteredNumber!=1:
     EnteredNumber=collatz(EnteredNumber)
     continue
 print("Well,the number you entered has been calculated to 1 finally")
+
+#Case2_逗号代码
+def Confun(list):
+    n=len(list)
+    i=0
+    x='answer is '
+    while i<n-1:
+        x=x+str(list[i])+', '
+        i=i+1
+    if i==n-1:
+        x=x+"and"+str(list[i])
+    return x
+spam=['apples','bananas','tofu','cats']
+print(Confun(spam))
