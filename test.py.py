6623b0c794e5ec0b971595a0fb21a3d8eebec394
ATmingfan <80809662+ATmingfan@users.noreply.github.com>
2021-08-05 17:04:58 +0800

    Update test.py.py

--- test.py.py
+++ test.py.py
@@ -144,3 +144,19 @@ for character in message:
     print(character)
     print(count[character])
 print(pprint.pformat(count))
+
+#!python3 
+#pw.py-An insecure password locker program
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+'luggage': '12345'}
+import pyperclip,sys
+if len(sys.argv)<2:
+    print('copy account password')
+    sys.exit()
+account=sys.argv[1]
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print(' Password for ' + account + ' copied to clipboard.')
+else:
+    print(' There is no account named ' + account)
