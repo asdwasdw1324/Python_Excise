cd21d8a32172c2cebc5c8f012d69ff43e0975703
ATmingfan <80809662+ATmingfan@users.noreply.github.com>
2021-10-21 23:08:16 +0800

    Update test.py.py

--- test.py.py
+++ test.py.py
@@ -495,3 +495,12 @@ try:
     res.raise_for_status()
 except Exception as error:
     print('There was a problem: %s'%(error))
+
+#DownloadtoFile
+import requests
+res=requests.get('http://www.gutenberg.org/cache/epub/1112/pg1112.txt')
+res.raise_for_status()
+playfile=open('RomeoAndJuliet.txt','wb')
+for chunk in res.iter_content(100000):
+    playfile.write(chunk)
+playfile.close()
